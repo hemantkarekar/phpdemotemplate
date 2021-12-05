7e7f6848afd66d695c0cfe91f50e1f145adabfad
HemantKarekar <hemant.v.karekar@gmail.com>
2021-12-05 23:38:15 +0530

    SEO metas added

--- components/_head.php
+++ components/_head.php
@@ -1,18 +1,29 @@
 <?php
 $meta = array();
 
+$owner = "Company Name";
+$site_name = "Product Name";
+
 $meta["author"] = "Hemant Karekar"; 
 $meta["copyright"] = "All rights Reserved. Developed by ".$meta["author"];
 
-$meta["keywords"] = "Web Developer, PHP Developer, Full Stack Developer (Linux, Apache, MySQL, PHP)"; 
-$meta["description"] = "Professionally, I am a Web Developer Executive and also a student, pursuing my Master in IT with a major in Information Security. That's all for now about my profession. As an individual, I think I am good at problem-solving skills, which is why I am an Enthusiastic Learner. I am always ready for challenges that make me think out of the box. Other than that, I do keep an interest in illustrations, digital arts. I have a noticeable performance in the field of Art."; 
+$title = "" . "|" . $site_name . "|" . $owner;
 
-$title = $meta["author"]."Hemant Karekar | Web & Application Developer";
 $meta["title"] = $title;
+$meta["keywords"] = ""; 
+$meta["description"] = "";
 ?>
+<!-- DEFAULT METAS -->
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+<!-- COMMON SEO TAGS -->
+<link type="text/plain" rel="author" href="/humans.txt" />
+<link rel="canonical" href="https://example.com/">
+<link rel="sitemap" type="application/xml" title="Sitemap" href="/sitemap.xml">
 
+<!-- VENDOR CSS LIBRARIES -->
+
+<!-- CUSTOM CSS -->
 <link rel="stylesheet" href="/assets/css/nav.css">
